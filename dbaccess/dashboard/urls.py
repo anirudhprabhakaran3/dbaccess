a31b60bbc9f6d2fe818bf36c621398163741814e
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-12-20 19:50:23 +0530

    Add admin navigation module

--- dbaccess/dashboard/urls.py
+++ dbaccess/dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from dashboard import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("dashboard/admin/", views.admin, name="admin_dashboard"),
 ]
--- dbaccess/dashboard/views.py
+++ dbaccess/dashboard/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 @login_required
 def index(request):
-    return render(request, "dashboard/index.html")
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return redirect("admin_dashboard")
+    else:
+        return render(request, "dashboard/index.html")
+
+
+@login_required
+def admin(request):
+    return render(request, "dashboard/admin.html")
